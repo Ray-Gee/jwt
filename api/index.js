d8e6e95a9c033a76a0db1b96c4038381950b8258
UedaRyuichi <uedaryuichi.sales@gmail.com>
2021-10-09 21:12:50 +0900

    寝ます

--- api/index.js
+++ api/index.js
@@ -1,34 +1,98 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
-app.use(express.json())
+const jwt = require("jsonwebtoken");
+app.use(express.json());
 
 const users = [
-    {
-        id: "1",
-        username: "john",
-        password: "john1",
-        isAdmin: true,
-    },
-    {
-        id: "2",
-        username: "jane",
-        password: "jane1",
-        isAdmin: false,
-    },
-]
+  {
+    id: "1",
+    username: "john",
+    password: "john1",
+    isAdmin: true,
+  },
+  {
+    id: "2",
+    username: "jane",
+    password: "jane1",
+    isAdmin: false,
+  },
+];
+
+let refreshTokens = [];
+
+app.post("/api/refresh", (req, res) => {
+  const refreshToken = req.body.token;
+
+  if (!refreshToken) return res.status(401).json("You are not authenticated");
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json("Refresh token is not valid");
+  }
+  jwt.verify(refreshToken, "myRefreshSecretKey", (err, user) => {
+    err && console.log(err);
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+  });
+});
+
+const generateAccessToken = (user) => {
+  return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "mySecretKey", {
+    expiresIn: "15m",
+  });
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "myRefreshSecretKey");
+};
 
 app.post("/api/login", (req, res) => {
-    const {username, password} = req.body
+  const { username, password } = req.body;
 
-    const user = users.find((u) => {
-        return u.username === username && u.password === password;
-    })
+  const user = users.find((u) => {
+    return u.username === username && u.password === password;
+  });
 
-    if (user) {
-        res.json(user)
-    } else {
-        res.status(400).json("Username or password incorrect")
-    }
-})
-app.listen(5000, () => console.log("backend")) 
+  if (user) {
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
+
+    res.json({
+      username: user.username,
+      isAdmin: user.isAdmin,
+      accessToken,
+    });
+  } else {
+    res.status(400).json("Username or password incorrect");
+  }
+});
+
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, "mySecretKey", (err, user) => {
+      if (err) {
+        return res.status(403).json("Token is not valid");
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json("You are not authenticated");
+  }
+};
+
+app.delete("/api/users/:userId", verify, (req, res) => {
+  if (req.user.id === req.params.userId || req.user.isAdmin) {
+    res.status(200).json("User has been deleted");
+  } else {
+    res.status(403).json("You are not allowed to delete this user");
+  }
+});
+app.listen(5000, () => console.log("backend"));
